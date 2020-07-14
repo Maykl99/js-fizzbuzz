626b646c54357fd3ca002dc580b8bd83cfb54a72
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-14 14:56:03 +0200

    app js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 
-for(var i=0; i<=100; i++){
+//variante 1 utilizzando una variabile locale
+/* for(var i=0; i<=100; i++){
     var outPut='';
     if(i % 3 ==0){
         outPut += 'Fizz';
@@ -12,3 +13,40 @@ for(var i=0; i<=100; i++){
     }
     console.log(outPut, i)
 }
+ */
+
+// variante 2 utilizzando variabili globali
+/* var name='Fizz';
+var name2='Buzz';
+var stampa=null;
+
+for(var i=0; i<=100; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        stampa=name+name2+ ' ' + i;
+    }
+    else if(i % 3 ==0){
+        stampa = name + ' ' + i;
+    }else if(i % 5 ==0){
+        stampa = name2 + ' ' + i;
+    }else{
+       stampa = i;
+    }
+
+    console.log(stampa)
+} */
+
+// variante 3 utilizzando il ciclo while
+
+var i=0;
+while(i<=100){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log('fizzBuzz', i)
+    }else if(i % 3 == 0){
+        console.log('fizz', i)
+    }else if(i % 5 == 0){
+        console.log('Buzz', i)
+    }else{
+        console.log(i)
+    }
+    i++;
+} 
